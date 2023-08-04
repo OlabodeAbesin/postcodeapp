09886cd15bc5831bd02c7f3b5e64cfbca3a687f6
Olabode Abesin <olaideolabode@ymail.com>
2023-08-04 10:39:25 +0100

    required tests package and wrote controller tests

---
+++ tests/Tests/PostcodeControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PostcodeControllerTest extends WebTestCase
+{
+    public function testPartialMatchAction()
+    {
+        $client = static::createClient();
+
+        // Test partial match for "LONDON"
+        $client->request('GET', '/api/postcodes/partial/LONDON');
+        $this->assertResponseIsSuccessful();
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertCount(2, $responseData);
+
+        // Assuming this app imported two postcodes with "LONDON" in their names
+        $this->assertSame('LONDON1', $responseData[0]['postcode']);
+        $this->assertSame('LONDON2', $responseData[1]['postcode']);
+    }
+
+    public function testNearbyAction()
+    {
+        $client = static::createClient();
+
+        // Test nearby postcodes for latitude 51.5074 and longitude -0.1278
+        $client->request('GET', '/api/postcodes/nearby/51.5074/-0.1278');
+        $this->assertResponseIsSuccessful();
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertCount(2, $responseData);
+
+        // Assuming this app imported two nearby postcodes
+        $this->assertSame('NEARBY1', $responseData[0]['postcode']);
+        $this->assertSame('NEARBY2', $responseData[1]['postcode']);
+    }
+}
